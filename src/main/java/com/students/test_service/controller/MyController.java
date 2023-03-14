3a02854d848f47ae96c1d79431d66fc1b59a9d8b
andrey kugubaev <egor.tolbaev@esplus.ru>
2023-03-14 11:50:48 +0500

    new class and change MyController

--- src/main/java/com/students/test_service/controller/MyController.java
+++ src/main/java/com/students/test_service/controller/MyController.java
@@ -1,5 +1,6 @@
 package com.students.test_service.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
@@ -8,14 +9,17 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import com.students.test_service.model.Request;
 import com.students.test_service.model.Response;
-
+import com.students.test_service.service.MyModifyService;
 import javax.validation.Valid;
-
+@Slf4j
 @RestController
 public class MyController {
 
     @PostMapping(value = "/feedback")
     public ResponseEntity<Response> feedback(@RequestBody Request request) {
+
+        log.info("Входящий request : " + String.valueOf(request));
+
         Response response = Response.builder()
                 .uid(request.getUid())
                 .operationUid(request.getOperationUid())
@@ -24,6 +28,9 @@ public class MyController {
                 .errorCode("")
                 .errorMessage("")
                 .build();
+
+        Response responseAfterModify = myModifyService.modify(response);
+        log.info("Исходящий response : " + String.valueOf(response));
 
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
